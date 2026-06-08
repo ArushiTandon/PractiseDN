e8c4e1f9fed2c60de52c04d18a610047d1817282
Arushi Tandon <arushitandon007@gmail.com>
2026-06-08 16:54:24 +0530

    Replace HomeController with AuthController and auth views
    Removed HomeController and its views. Added AuthController with Login and Register actions, along with corresponding views for authentication. Enabled static file serving and updated the default route to point to Auth/Login.

--- PractiseDN/Program.cs
+++ PractiseDN/Program.cs
@@ -9,10 +9,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseRouting();
+app.UseStaticFiles(); 
+
+//app.UseStaticFiles() enables the Static File Middleware in ASP.NET Core.
+//It allows files stored in the wwwroot folder, such as CSS, JavaScript, images, and fonts, to be served directly to the client.
+//Without it, requests for these files return 404 errors
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=First}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
